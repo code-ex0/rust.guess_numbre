61cc0cc9ecf2daeae4bd53c8543df2c3e485ea92
LSASSE <louis.sasse@ynov.com>
2021-03-31 17:33:24 +0200

    switch user

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(allocator_api)]
-#[warn(dead_code)]
 
+#[warn(dead_code)]
+#[warn(unused_must_use)]
 mod lib;
 
 extern crate savefile;
@@ -27,7 +28,7 @@ struct Number {
 struct Historic {
     won: bool,
     attempts: i32,
-    proposition: Vec<i32>,
+    propositions: Vec<i32>,
     game_duration: i64,
     number: Number,
 }
@@ -49,7 +50,8 @@ struct Player {
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize, Savefile)]
 struct Game {
-    players: Vec<Player>
+    players: Vec<Player>,
+    player_select: i32
 }
 
 
@@ -61,33 +63,26 @@ impl Number {
             limit_lower: 0,
         }
     }
-
-    pub fn get_number_to_find(&self) -> i32 {
+    fn get_number_to_find(&self) -> i32 {
         self.number_to_find
     }
-
     fn set_number_to_find(&mut self) {
         let mut rng = rand::thread_rng();
         let num: i32 = rng.gen_range(self.limit_lower..self.limit_upper);
         self.number_to_find = num;
     }
-
-    pub fn get_limit_upper(&self) -> i32 {
+    fn get_limit_upper(&self) -> i32 {
         self.limit_upper
     }
-
     fn set_limit_upper(&mut self, n: i32) {
         self.limit_upper = n;
     }
-
-    pub fn get_limit_lower(&self) -> i32 {
+    fn get_limit_lower(&self) -> i32 {
         self.limit_lower
     }
-
     fn set_limit_lower(&mut self, n: i32) {
         self.limit_lower = n;
     }
-
     fn compare_number(&self, user_number: i32) -> bool {
         let mut result: bool = false;
         if user_number < self.get_number_to_find() {
@@ -98,6 +93,24 @@ impl Number {
             result = true;
         }
         result
+    }
+}
+
+impl Historic {
+    fn get_won(&self) -> bool {
+        self.won
+    }
+    fn get_attempts(&self) -> i32 {
+        self.attempts
+    }
+    fn get_propositions(&self) -> &Vec<i32> {
+        &self.propositions
+    }
+    fn get_game_duration(&self) -> i64 {
+        self.game_duration
+    }
+    fn get_number(&self) -> &Number {
+        &self.number
     }
 }
 
@@ -121,21 +134,32 @@ impl Statistic {
 
 impl Player {
     fn new() -> Player {
+        let user_name = lib::input("║ What is your user name : ");
+        let age: i32 = loop {
+            let input = lib::ask_number("║ what is your age : ");
+            if input != -1 {
+                break input;
+            }
+            else {
+                println!("║ please enter a right answer");
+            }
+        };
         Player {
             id: 0,
-            user_name: lib::input("║ What is your user name : "),
-            age: lib::ask_number("║ what is your age : "),
+            user_name,
+            age,
             statistic: Default::default(),
         }
     }
     fn get_id(&self) -> i32 {
         self.id
     }
-
+    fn set_id(&mut self, id: i32) {
+        self.id = id
+    }
     fn get_user_name(&self) -> &str {
         &self.user_name
     }
-
     fn set_user_name(&mut self, user_name: String) {
         self.user_name = user_name;
     }
@@ -145,7 +169,6 @@ impl Player {
     fn set_age(&mut self, age: i32) {
         self.age = age
     }
-
     fn get_statistic(&self) -> &Statistic {
         &self.statistic
     }
@@ -158,7 +181,8 @@ impl Game {
             game.unwrap()
         } else {
             Game {
-                players: Default::default()
+                players: Default::default(),
+                player_select: 0
             }
         }
     }
@@ -168,11 +192,17 @@ impl Game {
     fn save_game(&self) {
         save_file("save-game.bin", 0, self).unwrap();
     }
-    fn get_player(&mut self, id: usize) -> &Player {
-        &mut self.players[id]
+    fn get_player(&mut self, id: i32) -> &mut Player {
+        &mut self.players[id as usize]
     }
     fn get_players(&self) -> &Vec<Player> {
         &self.players
+    }
+    fn get_player_select(&self) -> i32 {
+        self.player_select
+    }
+    fn set_player_select(&mut self, id: i32) {
+        self.player_select = id
     }
 }
 
@@ -183,27 +213,29 @@ fn load_game() -> Result<Game, SavefileError> {
 fn start_game(player: &mut Player) {
     let mut number = Number::new();
     number.set_number_to_find();
-    let mut proposition = vec![];
+    let mut propositions: Vec<i32> = vec![];
     let mut attempts = 1;
     let time = lib::get_time_ms();
 
     for i in 1..11 {
         println!("{0}", design(3));
-        let mut num: i32;
-        loop {
-            num = lib::ask_number("║ enter a new number : ");
-            if num != -1 {
-                break;
+        let num: i32 = loop {
+            let input = lib::ask_number("║ enter a new number : ");
+            if input != -1 {
+                break input;
             }
-        }
-        &proposition.push(num);
+            else {
+                println!("║ please enter a right answer");
+            }
+        };
+        &propositions.push(num);
         attempts = i;
         if number.compare_number(num) == true {
             break;
         }
     }
 
-    let won: bool = proposition.last().unwrap() == &number.get_number_to_find();
+    let won: bool = propositions.last().unwrap() == &number.get_number_to_find();
     if won == true {
         println!("║ you won in {} try", attempts);
         player.statistic.won_games += 1;
@@ -215,7 +247,7 @@ fn start_game(player: &mut Player) {
         Historic {
             won,
             attempts,
-            proposition,
+            propositions,
             game_duration: lib::get_time_ms() - time,
             number,
         }
@@ -225,15 +257,13 @@ fn start_game(player: &mut Player) {
 fn design(id: i32) -> &'static str {
     match id {
         1 => {
-            "╔═════════════════════════════════════════════════
-║ Welcome in my video games
+            "║ Welcome in my video games
 ╠═════════════════════════════════════════════════
 ║   1. -new user
 ║   2. -change user
 ║   3. -start new game
 ║   4. -statistic
-║  /q. -exit program
-╠═════════════════════════════════════════════════"
+║  /q. -exit program"
         }
         2 => {
             "╔═════════════════════════════════════════════════"
@@ -250,44 +280,90 @@ fn design(id: i32) -> &'static str {
     }
 }
 
+fn new_player(game: &mut Game) {
+    let mut player = Player::new();
+    if !&game.players.is_empty() {
+        player.set_id(game.get_players().last().unwrap().get_id() + 1);
+    }
+    game.new_player(player);
+    for i in &game.players {
+        println!("║ {0}-user name - {1}", i.get_id(), i.get_user_name())
+    }
+}
+
+fn choose_player(game: &mut Game) {
+    println!("{0}", design(3));
+    println!("║ switch user");
+    for user in game.get_players() {
+        println!("║ {} -- {}", user.get_id(), user.get_user_name());
+    }
+    loop {
+        let input = lib::ask_number("║ choose a number for select a user : ");
+        if input != -1 {
+            if (game.get_players().first().unwrap().get_id()..game.get_players().last().unwrap().get_id() + 1).contains(&input) {
+                game.set_player_select(input);
+                break
+            }
+        }
+    }
+
+
+}
+
+fn print_statistics(game: &mut Game) {
+    if !&game.get_players().is_empty() {
+        for i in game.get_players() {
+            let mut x: i32 = 0;
+            let mut average_try: i32 = 0;
+            let mut propositions: Vec<i32> = vec![];
+            for j in i.get_statistic().get_historic() {
+                x += 1;
+                average_try += j.get_attempts();
+                for k in j.get_propositions() {
+                    propositions.push(*k)
+                }
+            };
+            println!("║- {} {} -- {} -- {} -- {} -- {} --",
+                     i.get_id(),
+                     i.get_user_name(), i.get_statistic().get_all_game(),
+                     i.get_statistic().get_won_game(),
+                     i.get_statistic().get_won_game() as f32 / i.get_statistic().get_all_game() as f32,
+                     average_try as f32 / x as f32)
+        }
+    } else {
+        println!("║ no player has been created")
+    }
+}
+
 fn menu(game: &mut Game) {
-    println!("{0}", design(1));
+    println!("{0}\n{1}\n{2}", design(2), design(1), design(3));
     loop {
         match lib::input("║ choose a number for make action : ").as_str() {
             "1" => {
-                game.new_player(Player::new());
-                for i in &game.players {
-                    println!("║ {0}-user name - {1}", i.get_id(), i.get_user_name())
-                }
+                new_player(game);
             }
-            "2" => {}
+            "2" => {
+                choose_player(game);
+            }
             "3" => {
-                start_game(&mut game.get_player(0).clone())
+                if !&game.get_players().is_empty() {
+                    start_game(game.get_player(game.get_player_select()));
+                }
+                else {
+                    println!("║ not player has been created");
+                }
             }
             "4" => {
-                if !&game.players.is_empty() {
-                    for i in &game.players {
-                        let mut x: i32 = 0;
-                        let mut average_try: i32 = 0;
-                        let mut proposition = vec![];
-                        for j in &i.statistic.historic {
-                            x += 1;
-                            average_try += j.attempts;
-                            for k in &j.proposition {
-                                proposition.push(k)
-                            }
-                        };
-                        println!("║- {} -- {} -- {} -- {} -- {} --", i.user_name, i.statistic.all_games, i.statistic.won_games, i.statistic.won_games as f32 / i.statistic.all_games as f32, average_try as f32 / x as f32)
-                    }
-                } else {
-                    println!("║ no player has been created")
-                }
+                print_statistics(game);
             }
             "/q" => {
                 break;
             }
-            _ => {}
+            _ => {
+                println!("{0}", design(1));
+            }
         }
+        game.save_game();
         println!("{0}", design(3));
     }
     println!("{0}", design(4));
@@ -297,4 +373,21 @@ fn main() {
     let mut game = Game::new();
     menu(&mut game);
     game.save_game();
+}
+
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test(){
+        assert_eq!(0, 0)
+    }
+
 }
